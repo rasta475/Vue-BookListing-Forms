efb3159026b038ea3ac9025b7c3b559c9593fe82
Rob Rastelli <rrastelli@pcgus.com>
2020-03-04 15:55:06 -0500

    Changes for module 4

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
     <h1>{{title}}</h1>
+    <input v-model="searchInput" type="text" name="searchInput" placeholder="Search Books" />
     <ul>
-      <book-item v-for='book in books' :key='book.id' :book='book'></book-item>
+      <book-item v-for='book in searchedBooks' :key='book.id' :book='book'></book-item>
     </ul>
     <br><hr>
     <h2>Filtered Books By Ownership</h2>
@@ -37,6 +38,7 @@ export default {
       ],
       filters: ["bought", "borrowed"],
       holding: "bought",
+      searchInput: "",
     };
   },
   components: {
@@ -46,6 +48,12 @@ export default {
   computed: {
     filteredBooks() {
       return _.filter(this.books, ["ownership", this.holding]);
+    },
+    searchedBooks() {
+      const searchFilter = book => {
+        return book.title.toLowerCase().match(this.searchInput.toLowerCase());
+      };
+      return _.filter(this.books, searchFilter);
     }
   },
   methods: {
